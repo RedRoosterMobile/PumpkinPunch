1a0fc9803f3e9d3eb776cf6e99476e0f3a1553d7
Mojo Major <info@redroostermobile.com>
2024-12-08 13:26:52 +0100

    flame, ghost, decal, text, xr scene for later, android sdk stuff

--- testing_map.gd
+++ testing_map.gd
@@ -6,23 +6,38 @@ extends Node
 class Pumpkin:
 	var node: Node3D
 	var start_position: Vector3
-	var is_splattable: bool
 
 var pumpkins:Array[Pumpkin]= []
 var time:float = 0.0
 var start_pos:Vector3
+
+const PUMPKIN_Z:float = -6.0 # back/front
+const PUMPKIN_X:float = 1.1 # left/right
+const PUMPKIN_Y:float = 1 # up/down
+
+# use this for spawning
+const SPAWN_THING = preload("res://models/pumpkin_hollow_full_modified.tscn")
+const SPAWN_THING_BROKEN = preload("res://models/pumpkin_hollow_pieces_modified.tscn")
+# use this for initial positions
+var pumpkin_start_positions: Array[Vector3] = [
+	Vector3(-1.26961, 1.08145, -2.11638),
+	Vector3(0.960844, 1.08145, -2.11638),
+	Vector3(-0.062555, 0.91448, -0.284467),
+	Vector3(-1.18586, 1.08145, -5.71892),
+	Vector3(1.11298, 1.08145, -5.71892)
+]
 func _ready() -> void:
 	for thing:Node in get_children():
 		if thing.is_in_group("pumpkins"):
 			var p = Pumpkin.new()
 			p.node = thing
 			p.start_position = thing.position
-			p.is_splattable = false
+			# pumpkin_start_positions.append(thing.position)
 			pumpkins.append(p)
 			thing.scale *= 0.5
 			start_pos = thing.position
 			
-	print(pumpkins)
+	print(pumpkin_start_positions)
 
 func _physics_process(delta: float) -> void:
 	time += delta
@@ -41,27 +56,33 @@ func _physics_process(delta: float) -> void:
 		pumpkin.node.position.y += sinner/300 
 		
 		if pumpkin.node.position.z > 3:
-			pumpkin.node.position.z=-5.719
-		elif Input.is_action_just_pressed("ui_accept"):
-			# check if in kill zone
-			if pumpkin.is_splattable:
-				pumpkin.node.splat()
+			pumpkin.node.position.z = PUMPKIN_Z
 func create_new_pumpkin():
 	if Input.is_action_just_pressed("spawn"):
 		print("spawn")
+		var pumpkin = SPAWN_THING.instantiate()
+		var pumpkin_pieces = SPAWN_THING_BROKEN
+		pumpkin.scale *= 0.5
+		pumpkin.rotate_y(30) # do some deg2rad things
+		pumpkin.position = pumpkin_start_positions[0]
+		pumpkin.broken_model = pumpkin_pieces
+		add_child(pumpkin)
+		var p = Pumpkin.new()
+		p.node = pumpkin
+		p.start_position = pumpkin.position
+		pumpkins.append(p)
 	pass
-
+	
 func _on_kill_zone_area_entered(area: Area3D) -> void:
 	var pumpkin_node = area.get_parent()
 	var pp = pumpkins.filter(func(pumpkin):
 		return pumpkin.node == pumpkin_node
 	)
-	var p: Pumpkin = pp[0]
-	p.is_splattable = true
-	var mi:MeshInstance3D = p.node.get_child(0)
+	#var p: Pumpkin = pp[0]
+	#var mi:MeshInstance3D = p.node.get_child(0)
 	#mi.get_active_material()
-	var m : StandardMaterial3D = mi.get_active_material(0)
-	var m2 : StandardMaterial3D = mi.get_active_material(1)
+	#var m : StandardMaterial3D = mi.get_active_material(0)
+	#var m2 : StandardMaterial3D = mi.get_active_material(1)
 	#m.transparency=BaseMaterial3D.TRANSPARENCY_ALPHA;
 	#m2.transparency=BaseMaterial3D.TRANSPARENCY_ALPHA;
 	#print(mi)
@@ -72,9 +93,8 @@ func _on_kill_zone_area_exited(area: Area3D) -> void:
 	var pp = pumpkins.filter(func(pumpkin):
 		return pumpkin.node == pumpkin_node
 	)
-	var p: Pumpkin = pp[0]
+	#var p: Pumpkin = pp[0]
 	#print(p.node)
-	p.is_splattable = false
 	
 func follow_mouse():
 	var camera = get_viewport().get_camera_3d()
@@ -90,8 +110,8 @@ func follow_mouse():
 	pointer.position = intersection_point
 	
 	
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		print("splatt")
+	#if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+	#	print("splatt")
 	#pointer.position=lerp(pointer.position,intersection_point,0.001)
 
 func _on_hand_body_entered(body: Node) -> void:
@@ -100,5 +120,26 @@ func _on_hand_body_entered(body: Node) -> void:
 
 func _on_hand_area_3d_area_entered(area: Area3D) -> void:
 	print("ball hit")
-	print(area)
-	pass # Replace with function body.
+	print(area.get_parent())
+	area.get_parent().splat()
+
+func spawn_pumpkin():
+	# z -6
+	# x 1.1 / -1.11
+	pass
+	
+# dynamically load pumpkins:
+# on ready:
+# - spawn pumpkins with position
+# - add to array of custom class
+# process:
+# - filter destroyed pumpkins
+# - add more if below limit
+# - move them forward or reset if behind threshold
+# physica handler of glove:
+# - delete touched pumpkin
+# - delete reference from array
+
+# with music:
+# - spawn them on midi notes
+# - TBD
