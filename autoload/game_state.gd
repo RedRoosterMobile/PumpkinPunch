8a1b6d44d4c3c845fde9018ea67e6c3a05e4fd02
Mojo Major <info@redroostermobile.com>
2025-03-08 15:35:34 +0100

    cleanup

---
+++ autoload/game_state.gd
@@ -0,0 +1,11 @@
+class_name GameState extends Node
+
+
+# when game is ready to spawn pumpkins
+var game_started:bool = false
+
+var game_finished:bool = false
+
+var skeleton_resurrected:bool = false
+var skeleton_active:bool = false # game started??
+var skeleton_died:bool = false
---
+++ autoload/messenger.gd
@@ -0,0 +1,19 @@
+extends Node
+
+# do signals here
+# example
+signal screenshake(strength: float)
+
+# when game is ready to spawn pumpkins
+signal game_started()
+# when game ends (player die, win)
+signal game_finished()
+
+signal skeleton_killed()
+signal skeleton_died()
+
+signal pumpkin_spawned()
+signal pumpkin_collision_player()
+signal pumpkin_collision_hand()
+# when it hits the killzone behind the player
+signal pumpkin_auto_destroyed()
