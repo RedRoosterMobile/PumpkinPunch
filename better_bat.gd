3b1a84c5f891e65ef2251f8bab52549e41646307
Mojo Major <info@redroostermobile.com>
2025-03-13 12:38:24 +0100

    remove old bats

--- better_bat.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-
-func _ready() -> void:
-	print("current animation")
-	print(animation_player.current_animation)
-	animation_player.play("ArmatureAction")
