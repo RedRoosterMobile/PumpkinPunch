bd564072cc72f85c968a36076008ab5739350f46
Mojo Major <info@redroostermobile.com>
2025-03-14 17:11:32 +0100

    playing with path follow

--- models/bat_path_follow_3d.gd
+++ models/bat_path_follow_3d.gd
@@ -2,26 +2,26 @@ extends PathFollow3D
 
 @export var speed = 2.0  # Speed in units per second
 var animation_player: AnimationPlayer
+
 func _ready() -> void:
-	
-	animation_player=get_child(0).get_child(1)
+	animation_player = get_child(0).get_child(1)
+
 func _process(delta):
 	
-	if progress <= 0.01:
+	if progress_ratio <= 0.01:
 		#Audio.play("audio/crazy_bat.ogg", true, global_transform)
 		pass
 	# Increase the progress along the path
 	progress += speed * delta
 	
 	if (progress_ratio > 0.55):
+		# aka: flap wings
 		animation_player.play("ArmatureAction")
-	
-	#
 	
 	# Optional: Loop the path when the bat reaches the end
 	if progress_ratio >= 0.99:
-		print("resetting bat")
+		# resetting bat
 		animation_player.stop(false)
-		progress = 0.0
+		progress_ratio = 0.0
 		
 		
