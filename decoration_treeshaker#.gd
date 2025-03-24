926ec685e2d2cda5c75c189cb8f63f6b1460cf41
Mojo Major <info@redroostermobile.com>
2025-03-24 21:16:15 +0100

    some rearrangements. playing with lights

--- decoration_treeshaker#.gd
+++ decoration_treeshaker#.gd
@@ -2,15 +2,10 @@ extends Node3D
 
 @onready var tree_pine_orange_medium_3: Node3D = $tree_pine_orange_medium3
 @onready var tree_pine_orange_medium_2: Node3D = $tree_pine_orange_medium2
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-	
-	
-	
-
-
+@onready var tree_pine_orange_medium_4: Node3D = $tree_pine_orange_medium4
+@onready var tree_pine_orange_medium_5: Node3D = $tree_pine_orange_medium5
+@onready var tree_pine_yellow_medium_2: Node3D = $tree_pine_yellow_medium2
+# treeshake animation
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 var time: float = 0.0
 func _physics_process(delta: float) -> void:
@@ -18,6 +13,8 @@ func _physics_process(delta: float) -> void:
 	var sinner = sin(time)
 	
 	tree_pine_orange_medium_3.rotate_x(deg_to_rad(0.025*sinner))
+	tree_pine_orange_medium_5.rotate_x(deg_to_rad(0.038*sinner))
 	
 	var cosser = sin(time*0.9+5) 
 	tree_pine_orange_medium_2.rotate_x(deg_to_rad(0.035*cosser))
+	tree_pine_orange_medium_4.rotate_x(deg_to_rad(0.025*cosser))
